f706c68bde41e1e4cd2cf921fc173fa40ee917f9
Tariku <tarikufirdu@gmail.com>
2025-01-17 23:31:38 +0300

    backend fix

--- Backend/app.js
+++ Backend/app.js
@@ -1,30 +1,22 @@
-import cors from "cors";
-import express from "express"; 
-import { connectDB } from "./config/db.js";
-import userRoutes from "./routes/userRoutes.js";
-import postRoutes from "./routes/postRoutes.js";
-import notificationRoutes from "./routes/notificationRoutes.js";
- 
+// app.js
+import cors from 'cors'
+import express from 'express'
+import { connectDB } from './config/db.js'
+import  userRoutes  from './routes/userRoutes.js'
+import postRoutes from './routes/postRoutes.js'
+import notificationRoutes from './routes/notificationRoutes.js'
+const app = express()
+app.use(express.json())
+app.use(cors())
 
-const app = express();
+connectDB()
 
-// Middleware
-app.use(express.json());
 
-// CORS Configuration
-app.use(
-  cors()
-);
+app.use('/api', userRoutes)
+app.use('/api', postRoutes)
+app.use('/api', notificationRoutes)
 
-// Connect to Database
-connectDB();
-
-// API Routes
-app.use("/api/users", userRoutes);
-app.use("/api/posts", postRoutes);
-app.use("/api/notifications", notificationRoutes);
+export default app
 
  
- 
 
-export default app;
