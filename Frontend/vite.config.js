ddb8f91078ba72da62b8bc472b635c36f57695a3
Tariku <tarikufirdu@gmail.com>
2025-01-17 13:49:58 +0300

    vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3000,
+    historyApiFallback: true,
+    host: true,
+    strictPort: true,
+    cors: true,
+  },
+  preview: {
+    port: 3000,
+    strictPort: true,
+    host: true,
+    historyApiFallback: true,
+  },
+  build: {
+    outDir: "dist",
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  base: "/",
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+});
