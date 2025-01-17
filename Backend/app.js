7159224e612e5f09ad8782fa54489852a868f292
Tariku <tarikufirdu@gmail.com>
2025-01-17 23:18:21 +0300

    route note found fix

--- Backend/app.js
+++ Backend/app.js
@@ -25,13 +25,6 @@ app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
 
  
-// Error Handling
-app.use((req, res, next) => {
-  res.status(404).json({ message: "Route not found" });
-});
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ message: "Something went wrong", error: err.message });
-});
+ 
 
 export default app;
